dd309da09d45b4aec602cc95610eb2a964afe440
PantiraR818 <pantiraploy818@gmail.com>
2022-02-25 12:33:34 +0700

    v2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,34 +7,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   content = [{
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140051.png?token=exp=1645458320~hmac=73a044e64efeb398e1a94427bdfe883a",
-    img : "https://cdn-icons.flaticon.com/png/128/3414/premium/3414885.png?token=exp=1645456112~hmac=b37ba1bd1291f0d3b2d0fd7ebf70c82c",
+    avatar : "https://cdn-icons.flaticon.com/png/512/4140/premium/4140048.png?token=exp=1645507305~hmac=9a38018834926aa7c43e17bab760f6bd",
+    img : "https://cdn-icons.flaticon.com/png/128/3046/premium/3046948.png?token=exp=1645506593~hmac=7b3fc5b8f95faae8f7a4b8dd605f7382",
     name : "Ployly"
   },
   {
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645456766~hmac=0152a72c242249b0f90f12449e2106a0",
-    img : "https://cdn-icons.flaticon.com/png/512/1048/premium/1048315.png?token=exp=1645458600~hmac=ea2cff4f19b4aeafda36a4c6c63d49e9",
+    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645507142~hmac=d5b899be537224efb4b891ef7eb9725a",
+    img : "https://cdn-icons.flaticon.com/png/128/1048/premium/1048315.png?token=exp=1645506659~hmac=d7243b53117f3c379b2e48081b79211e",
     name : "MuchMhee"
    
   },
   {
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140051.png?token=exp=1645458320~hmac=73a044e64efeb398e1a94427bdfe883a",
-    img : "https://cdn-icons.flaticon.com/png/512/1698/premium/1698720.png?token=exp=1645458679~hmac=40efabbfadf3339994c449981b344247",
+    avatar : "https://cdn-icons.flaticon.com/png/512/4140/premium/4140048.png?token=exp=1645507305~hmac=9a38018834926aa7c43e17bab760f6bd",
+    img : "https://cdn-icons.flaticon.com/png/128/3414/premium/3414885.png?token=exp=1645506716~hmac=b37116afd8bb9f012009c2ea4a92ffaf",
     name : "MildMild"
   
   },
   {
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645456766~hmac=0152a72c242249b0f90f12449e2106a0",
-    img : "https://cdn-icons.flaticon.com/png/512/3037/premium/3037929.png?token=exp=1645457284~hmac=3abe7b03f8698fad1d241541d1575af0",
+    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645507142~hmac=d5b899be537224efb4b891ef7eb9725a",
+    img : "https://cdn-icons.flaticon.com/png/128/1048/premium/1048325.png?token=exp=1645506771~hmac=cdedb84b41904989c398df14accce4bc",
     name : "FahFah"
   },
   {
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140051.png?token=exp=1645458320~hmac=73a044e64efeb398e1a94427bdfe883a",
-    img : "https://cdn-icons.flaticon.com/png/512/3046/premium/3046948.png?token=exp=1645458641~hmac=1bc0ca88a78149ebc85c7388d7ec16eb",
+    avatar : "https://cdn-icons.flaticon.com/png/512/4140/premium/4140048.png?token=exp=1645507305~hmac=9a38018834926aa7c43e17bab760f6bd",
+    img : "https://cdn-icons.flaticon.com/png/128/551/premium/551239.png?token=exp=1645506849~hmac=98a9a51a7a69592eb15146af64943b2f",
     name : "Elle"
   },
   {
-    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645456766~hmac=0152a72c242249b0f90f12449e2106a0",
+    avatar : "https://cdn-icons.flaticon.com/png/128/4140/premium/4140047.png?token=exp=1645507142~hmac=d5b899be537224efb4b891ef7eb9725a",
     img : "https://cdn-icons-png.flaticon.com/128/3094/3094972.png",
     name : "Somsom"
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,5 +8,45 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+  news = [
+    {
+      img: "https://cdn-icons.flaticon.com/png/128/3046/premium/3046948.png?token=exp=1645506593~hmac=7b3fc5b8f95faae8f7a4b8dd605f7382",
+      title: "user1"
+    },
+    {
+      img: "https://cdn-icons.flaticon.com/png/128/1048/premium/1048315.png?token=exp=1645506659~hmac=d7243b53117f3c379b2e48081b79211e",
+      title: "user2"
+    },
+    {
+      img: "https://cdn-icons.flaticon.com/png/128/3046/premium/3046948.png?token=exp=1645506593~hmac=7b3fc5b8f95faae8f7a4b8dd605f7382",
+      title: "user3"
+    },
+    {
+      img: "https://cdn-icons-png.flaticon.com/128/3094/3094972.png",
+      title: "user4"
+    },{
+      img: "https://cdn-icons.flaticon.com/png/128/1048/premium/1048325.png?token=exp=1645506771~hmac=cdedb84b41904989c398df14accce4bc",
+      title: "user5"
+    },
+    {
+      img: "https://cdn-icons.flaticon.com/png/128/1048/premium/1048315.png?token=exp=1645506659~hmac=d7243b53117f3c379b2e48081b79211e",
+      title: "user6"
+    },{
+      img: "https://cdn-icons.flaticon.com/png/128/551/premium/551239.png?token=exp=1645506849~hmac=98a9a51a7a69592eb15146af64943b2f",
+      title: "user7"
+    },
+    {
+      img: "https://cdn-icons.flaticon.com/png/128/1048/premium/1048325.png?token=exp=1645506771~hmac=cdedb84b41904989c398df14accce4bc",
+      title: "user8"
+    },{
+      img: "https://cdn-icons.flaticon.com/png/128/1048/premium/1048315.png?token=exp=1645506659~hmac=d7243b53117f3c379b2e48081b79211e",
+      title: "user9"
+    },
+    {
+      img: "https://cdn-icons-png.flaticon.com/128/3094/3094972.png",
+      title: "user10"
+    },
+
+  ]
 
 }
